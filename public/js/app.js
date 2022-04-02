18c6cf25d3044dc947f3f7401bb625af82cdb380
Jérémie <jeremie.bonal@gmail.com>
2022-04-02 23:05:35 +0200

    Rewrote app to get rid of jQuery and to be more easily configured.

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,43 @@
-const cards = {
-  "A": "Hello",
-  "B": "Bonjour",
-  "C": "Hola",
+const $ = (sel) => { return document.querySelector(sel) }
+
+const cards = getJSON('assets/data.json');
+
+function getJSON(url, qs_params) {
+  function buildQueryString(params) {
+    return Object.entries(params).map(d => `${d[0]}=${d[1]}`).join('&');
+  }
+
+  return new Promise((resolve, reject) => {
+    const qs = qs_params ? '?' + buildQueryString(qs_params) : '';
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', `${url}${qs}`);
+
+    xhr.onload = function() {
+      if (xhr.status >= 200 && xhr.status < 400) {
+        resolve(JSON.parse(xhr.responseText));
+      } else {
+        resolve(xhr.responseText);
+      }
+    };
+    xhr.onerror = () => reject(xhr.statusText);
+    xhr.send();
+  });
 }
 
-function randomize() {
-  const currentCard = $("#card").data('selected');
-  const [cardName, description] = pickRandomFrom(omit(cards, currentCard));
-  const newUrl = "https://via.placeholder.com/300.png/09f/fff?text="+cardName;
-  $("#card #picture").attr("src", newUrl);
-  $("#card #picture").attr("alt", cardName);
-  $("#card #description").text(description);
-  $("#card").data('selected', cardName);
-}
-
-function omit(obj, omitKey) {
-  return Object.keys(obj).reduce((result, key) => {
-    if(key !== omitKey) {
-       result[key] = obj[key];
-    }
-    return result;
-  }, {});
+const randomize = async() => {
+  const currentCard = $("#card").getAttribute('data-selected');
+  const eligibleCards = (await cards).filter(card => card.name !== currentCard)
+  const { name: cardName, description, imageUrl: newUrl } = pickRandomFrom(eligibleCards);
+  $("#card #picture").setAttribute("src", newUrl);
+  $("#card #picture").setAttribute("alt", cardName);
+  $("#card #description").innerHTML = description;
+  $("#card").setAttribute('data-selected', cardName);
 }
 
 function pickRandomFrom(collection) {
   const keys = Object.keys(collection)
   const prop = keys[Math.floor(Math.random() * keys.length)]
   const value = collection[prop];
-  return [prop, value]
+  return value;
 }
+
